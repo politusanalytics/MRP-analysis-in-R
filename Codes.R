95ad02f3262c4801a78ef06788a06600b207aaab
M. Fuat Kına <47318027+fuatkina@users.noreply.github.com>
2022-03-07 13:05:11 +0300

    Update Codes.R

--- Codes.R
+++ Codes.R
@@ -12,25 +12,30 @@ library(arm)
 library(extrafont)
 library(readxl)
 
-#Two input datasets
-user_data <- read_excel("married data from twitter.xlsx")
-pop_data <- read_excel("pop data tüik as matrix.xlsx")
+#Two input datasets:
+user_data <- read_excel("")
+pop_data <- read_excel("")
 
+#Multi-level regression model:
 model <- glmer(dep_var ~ var_context + (1|gender) + (1|age) + (1|var3) + (1|var_geo), data= user_data, family=binomial("probit"))
 
+#Realizations of the random effects:
 re.gender <- ranef(model)$gender[[1]]
 re.age <- ranef(model)$age[[1]]
 re.var3 <- ranef(model)$var3[[1]]
 re.var_geo <- ranef(model)$var_geo[[1]]
 
 #We consider gender has two subcategories. The numbers of subcategories for other variables are three numeric inputs of the model.
+#Three input parameters:
 N_age <-
 N_3 <- 
 N_geo <-
-  
+
+#Calculate total numbers of combinations.:
 N_cat <- 2 * N_age * N_3
 N_total <- N_cat * N_geo
 
+#Repeat the realizations such that all combinations are given, in the same order with Table 1 (in the Summary document):
 gender.re <- rep(re.gender,2*N_3)
 age.re <- rep(kronecker(re.age,c(1,1)), N_3)
 var3.re <- kronecker(re.var3,rep(1, N_1*2))
@@ -38,8 +43,10 @@ ind.re <- rowSums(cbind(re.gender, re.age, re.var3))
 ind.re <- ind.re + fixef(model)[1]
 beta1 <- fixef(model)[2]
 
+#Produce a list for var_context, grouped at geographical level:
 var_context <- user_data %>% group_by(var_geo) %>% summarise(v_c=mean(var_context)) %>% .$v_c
 
+#Create a vector of length N_cat * N_geo:
 y.lat <- rep(NA,N_total)
 for (i in 1:N_geo){
   a <- ((i-1)*N_cat)+1
@@ -47,10 +54,13 @@ for (i in 1:N_geo){
   y.lat[a:b] <- ind.re + beta1 * var_context[i] + re.var_geo[i]
 }
 
+#Transform those to predicted probabilities:
 p <- pnorm(y.lat)
 
+#Calculate total average:
 total_average <- sum(p*pop_data)/sum(pop_data)
 
+#Create a list for the averages at the level of geographical units:
 list_of_averages_for_units <- rep(NA,N_geo)
 for (i in 1:N_geo){
   a1 <- ((i-1)*N_cat)+1
@@ -60,5 +70,4 @@ for (i in 1:N_geo){
   list_of_averages_for_units[i] <-  sum(p*a)/sum(a)
 }
 
-#The two outputs will be total_average and list_of_averages_for_units!
-
+#The two outputs are total_average and list_of_averages_for_units!
